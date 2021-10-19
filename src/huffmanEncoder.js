d26b3fec92a7296004ed0a6a52986c57b3c56e51
Louis Andrew <louis.andrew@share-now.com>
2021-10-19 20:20:01 +0200

    style: fix whitespace

--- src/huffmanEncoder.js
+++ src/huffmanEncoder.js
@@ -4,19 +4,19 @@ const EMPTY_NODE_VALUE = -1;
 
 export default class HuffmanEncoder {
     /**
-   * Helper function to sort huffman nodes by its count in a descending manner.
-   * @param {HuffmanNode} nodeA First huffman node.
-   * @param {HuffmanNode} nodeB Second huffman node.
-   */
+     * Helper function to sort huffman nodes by its count in a descending manner.
+     * @param {HuffmanNode} nodeA First huffman node.
+     * @param {HuffmanNode} nodeB Second huffman node.
+     */
     static byCountDescending(nodeA, nodeB) {
         return nodeA.count - nodeB.count;
     }
 
     /**
-   * Static method creates a huffman tree from symbols.
-   * @param {[number, number][]} symbols Array with pairs of symbol value and symbol count.
-   * @returns {HuffmanNode} Root node of the huffman tree.
-   */
+     * Static method creates a huffman tree from symbols.
+     * @param {[number, number][]} symbols Array with pairs of symbol value and symbol count.
+     * @returns {HuffmanNode} Root node of the huffman tree.
+     */
     static getTree(symbols) {
         const nodes = symbols.map(
             ([value, count]) => new HuffmanNode({ value, count }),
@@ -40,10 +40,10 @@ export default class HuffmanEncoder {
     }
 
     /**
-   * Static method creates an array with codes for every symbol in the Huffman tree.
-   * @param {HuffmanNode} rootNode Root node of a huffman tree.
-   * @returns {Map<number, string>} Map of symbol values with corresponding binary codes.
-   */
+     * Static method creates an array with codes for every symbol in the Huffman tree.
+     * @param {HuffmanNode} rootNode Root node of a huffman tree.
+     * @returns {Map<number, string>} Map of symbol values with corresponding binary codes.
+     */
     static getCodes(rootNode) {
         const codes = new Map();
         this.getPathForNode(rootNode, codes, '');
@@ -52,11 +52,11 @@ export default class HuffmanEncoder {
     }
 
     /**
-   * Recursive function to get a path of a node.
-   * @param {HuffmanNode} root Root node of a huffman tree.
-   * @param {Map<number, string>} map Map of symbol values with corresponding binary codes.
-   * @param {string} path Path taken to the current root node.
-   */
+     * Recursive function to get a path of a node.
+     * @param {HuffmanNode} root Root node of a huffman tree.
+     * @param {Map<number, string>} map Map of symbol values with corresponding binary codes.
+     * @param {string} path Path taken to the current root node.
+     */
     static getPathForNode(root, map, path) {
         const { left, right, value } = root;
         const isEndNode = !left && !right;
@@ -75,11 +75,11 @@ export default class HuffmanEncoder {
     }
 
     /**
-   * Static method encodes the vlc code pairs and the source data itself.
-   * @param {Map<number, string>} codes Map of symbol values with corresponding binary codes.
-   * @param {DataView} sourceData Source data to be encoded using the code pairs.
-   * @returns {string} Huffman encoded binary data as a string of bits.
-   */
+     * Static method encodes the vlc code pairs and the source data itself.
+     * @param {Map<number, string>} codes Map of symbol values with corresponding binary codes.
+     * @param {DataView} sourceData Source data to be encoded using the code pairs.
+     * @returns {string} Huffman encoded binary data as a string of bits.
+     */
     static encodeData(codes, sourceData) {
         const sourceBytes = Array.from(Array(sourceData.byteLength)).map(
             (_, index) => sourceData.getUint8(index),
@@ -94,31 +94,31 @@ export default class HuffmanEncoder {
     }
 
     /**
-   * Helper function to convert numeric value to Bits with predefined length.
-   * @param {number} length Length of the converted value in Bits.
-   * @param {number} value Value of the number to be converted in Bits.
-   * @returns {string}
-   */
+     * Helper function to convert numeric value to Bits with predefined length.
+     * @param {number} length Length of the converted value in Bits.
+     * @param {number} value Value of the number to be converted in Bits.
+     * @returns {string}
+     */
     static toBits(length, value) {
         return value.toString(2).padStart(length, '0');
     }
 
     /**
-   * Helper function to cretae symbol code pair required for the VLC header
-   * @param {number} symbol Value of the symbol.
-   * @param {string} code Encoded code for the symbol as result of huffman encoding.
-   * @returns {string} Symbol-Code pair (See README.md file).
-   */
+     * Helper function to cretae symbol code pair required for the VLC header
+     * @param {number} symbol Value of the symbol.
+     * @param {string} code Encoded code for the symbol as result of huffman encoding.
+     * @returns {string} Symbol-Code pair (See README.md file).
+     */
     static createSymbolCodePair(symbol, code) {
         const codeLength = code.length;
         return `${this.toBits(8, codeLength)}${this.toBits(8, symbol)}${code}`;
     }
 
     /**
-   * Helper function to create VLC header for the Huffman-Binary data
-   * @param {Map<number, string>} codes Map of symbol values with corresponding binary codes.
-   * @returns {string}
-   */
+     * Helper function to create VLC header for the Huffman-Binary data
+     * @param {Map<number, string>} codes Map of symbol values with corresponding binary codes.
+     * @returns {string}
+     */
     static createVLCHeader(codes) {
         return [...codes.entries()]
             .map(([symbol, code]) => this.createSymbolCodePair(symbol, code))
